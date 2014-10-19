ba925ab8563973e555c0c323d1d852e9a0357843
Netzcream <netzcream@gmail.com>
2014-10-18 22:31:44 -0300

    Buscar

--- index.php
+++ index.php
@@ -13,8 +13,7 @@ if (!isset($_SESSION['location'])) {
 	<html>
 	<head>
 		<title>Bolsa DV</title>
-		
-		<meta charset="ISO-8859-1">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 		
 		<!-- Distintas hojas de estilos utilizadas -->
